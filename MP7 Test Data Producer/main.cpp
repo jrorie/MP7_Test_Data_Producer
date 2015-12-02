08030531e9eb8e3f547f91d2335d89cb1368fd4f
Jamal Tildon Rorie <jrorie@rice.edu>
2015-12-02 15:58:25 -0600

    Initial Commit

---
+++ MP7 Test Data Producer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MP7 Test Data Producer
+//
+//  Created by Jamal Rorie on 12/2/15.
+//  Copyright © 2015 Rice. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
